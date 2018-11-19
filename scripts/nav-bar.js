2cdc99251406442ef20b9cb147dea34e1e7dab98
unknown <moizhus123@gmail.com>
2018-11-20 01:17:47 +0500

    corrected CSS of Logo

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -8,12 +8,10 @@ var navbarKiInnerHTML = `<div class="container">
   
 </div>
 <div class="collapse navbar-collapse" id="myNavbar">
+  <a href="./index.html" style="padding: 0px">
+    <img src="final-logo.png" alt="logo" style="width:120px;height:120px;" />
+  </a>
   <ul class="nav navbar-nav navbar-right">
-  <li id="logo">
-            <a href="./index.html" style="padding: 0px">
-              <img src="final-logo.png" alt="logo" style="width:120px;height:120px;" />
-            </a>
-          </li>
     <li class="a"><a href="./index.html">Home</a></li>
     <li class="a"><a href="#">Find Universities</a></li>
     <li class="a"><a href="./contacts.html">Blog</a></li>
@@ -25,8 +23,9 @@ var navbarKiInnerHTML = `<div class="container">
 var styleTag = `<link rel="stylesheet" href="./styles/custom.css">`;
 
 function createNavBar() {
-
-  document.getElementsByTagName("head")[0].innerHTML += `<link rel="icon" href="final-logo.png">`;
+  document.getElementsByTagName(
+    "head"
+  )[0].innerHTML += `<link rel="icon" href="final-logo.png">`;
   var parent = document.getElementsByTagName("body")[0];
   var firstElement = parent.firstElementChild;
   let newElement = document.createElement("nav");
