42f17bfbd2970835a9aa8f9e28e2128ea03bbe78
Affan Khan <affanhk95@hotmail.com>
2018-11-19 18:38:27 +0500

    admin page

---
+++ scripts/init-firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDNxYGW_59-n9J9P0eTnnLsIzt_Oh7K1HI",
+  authDomain: "career-counseling-fyp.firebaseapp.com",
+  databaseURL: "https://career-counseling-fyp.firebaseio.com",
+  projectId: "career-counseling-fyp",
+  storageBucket: "career-counseling-fyp.appspot.com",
+  messagingSenderId: "476501772836"
+};
+firebase.initializeApp(config);
---
+++ scripts/nav-bar.js
@@ -0,0 +1,38 @@
+var navbarKiInnerHTML = `<div class="container">
+<div class="navbar-header">
+  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+  </button>
+  
+</div>
+<div class="collapse navbar-collapse" id="myNavbar">
+  <ul class="nav navbar-nav navbar-right">
+  <li id="logo">
+            <a href="./index.html" style="padding: 0px">
+              <img src="final-logo.png" alt="logo" style="width:120px;height:120px;" />
+            </a>
+          </li>
+    <li class="a"><a href="./index.html">Home</a></li>
+    <li class="a"><a href="#">Find Universities</a></li>
+    <li class="a"><a href="./contacts.html">Blog</a></li>
+    <li class="a"><a href="./about.html">About Us</a></li>
+    <li class="a"><a href="./login.html">Login | SignUp</a></li>
+  </ul>
+</div>
+</div>`;
+var styleTag = `<link rel="stylesheet" href="./styles/custom.css">`;
+
+function createNavBar() {
+
+  document.getElementsByTagName("head")[0].innerHTML += `<link rel="icon" href="final-logo.png">`;
+  var parent = document.getElementsByTagName("body")[0];
+  var firstElement = parent.firstElementChild;
+  let newElement = document.createElement("nav");
+  newElement.setAttribute("class", "navbar navbar-default");
+  newElement.innerHTML = navbarKiInnerHTML;
+  parent.insertBefore(newElement, firstElement);
+}
+
+createNavBar();
